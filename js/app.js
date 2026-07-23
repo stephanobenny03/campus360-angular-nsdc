38d9492d7ebf028ab569a8499f2dd46ee2a00659
stephanobenny03 <stephanobenny03gmail.com>
2026-07-23 09:15:20 +0530

    campus360 v1

---
+++ js/app.js
@@ -0,0 +1,63 @@
+var app = angular.module("campus360", []);
+
+app.controller("dashboardcontroller", function ($scope) {
+
+    // College Details
+    $scope.collegename = "FISAT";
+    $scope.totalStudents = 1200;
+    $scope.totalFaculty = 85;
+
+    // Student List
+    $scope.students = [
+        "Chandler",
+        "Joey",
+        "Monica",
+        "Phoebe",
+        "Steph"
+    ];
+
+    // Filter Demo Data
+    $scope.fees = 25000;
+    $scope.today = new Date();
+
+    // Department Details
+    $scope.departments = [
+        "IMCA",
+        "MCA",
+        "CSD",
+        "BSC",
+        "BBA"
+    ];
+
+    $scope.selectedDepartments = "MCA";
+
+    // ng-disabled Demo
+    $scope.isDisabled = true;
+
+    $scope.allowAdmission = function () {
+        $scope.isDisabled = false;
+    };
+
+    $scope.isReadOnly = true;
+
+    $scope.toggleReadOnly = function () {
+        $scope.isReadOnly = !$scope.isReadOnly;
+    };
+    
+
+    // Hide Student List Initially
+    $scope.showstudents = false;
+
+    // Add Student
+    $scope.addstudent = function () {
+        $scope.totalStudents++;
+    };
+
+    // Add Faculty
+    $scope.addfaculty = function () {
+        $scope.totalFaculty++;
+    };
+
+    console.log("Dashboard Controller Loaded");
+
+});
